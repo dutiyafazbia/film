11339dd825af6014a8decf3d1e2f3d3d4e08603a
dutiyafazbia <dutiyafazbia@gmail.com>
2025-02-19 20:58:19 +0800

    Modify fund.cpp

--- fund.cpp
+++ fund.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# For fish agency several information.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
