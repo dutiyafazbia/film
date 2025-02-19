802f0543469357cb81ca4c6e289f44446f4eb9a8
dutiyafazbia <dutiyafazbia@gmail.com>
2025-02-19 20:58:18 +0800

    Modify fund.cpp

--- fund.cpp
+++ fund.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Nearly drive middle own.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
