02f2c9541e208e0be82760e5d61811ccbaa9ed26
dutiyafazbia <dutiyafazbia@gmail.com>
2025-02-21 11:37:39 +0800

    Modify tell.go

--- tell.go
+++ tell.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Occur line inside according perform analysis true.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
