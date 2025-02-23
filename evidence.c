f91d8e88b2fbf83c28dd3bc2a471bc85dff456ab
dutiyafazbia <dutiyafazbia@gmail.com>
2025-02-23 15:21:46 +0800

    Modify evidence.c

--- evidence.c
+++ evidence.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Radio so cover evidence style theory control.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
