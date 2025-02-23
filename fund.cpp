ad42d3b6eff155cbc57e38e48a5a2159aa5852aa
dutiyafazbia <dutiyafazbia@gmail.com>
2025-02-23 15:21:46 +0800

    Modify fund.cpp

--- fund.cpp
+++ fund.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Understand name moment think.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
